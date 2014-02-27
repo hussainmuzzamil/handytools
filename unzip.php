ede77c55a869d77b05e7217e3d71e4c7e514dcd9
Muzzamil Hussain <cc.muzz@gmail.com>
2014-02-27 15:44:56 -0800

    unzip a zip file on a remote server

--- unzip.php
+++ unzip.php
@@ -1,4 +1,9 @@
 <?php 
+/*
+Very helpful in transferring huge number of files as a zip and then extract these over remote server.
+This tool will extract any zip file in to current directory of a remote web server with the same folder name. 
+
+*/
 $zip = new ZipArchive; 
     if ($zip->open("ivm.zip")){ 
         $path = getcwd() . "/"; 
