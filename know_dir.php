ba97986ff88553c8cc4f16dd8af1c05aa433fa72
Muzzamil Hussain <cc.muzz@gmail.com>
2014-02-27 12:03:48 -0800

    details related to file

--- know_dir.php
+++ know_dir.php
@@ -1,4 +1,8 @@
 <?php 
+/*
+most of the time when on remote server, we dont know what is the directory details where we are working
+at. this little file will let you know what is your current directory
+*/
 
 { 
         $path = getcwd() . "/"; 
