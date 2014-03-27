d4593ed6f0e0e8ad44513e1bcca2a5c0fbca37fb
Muzzamil Hussain <cc.muzz@gmail.com>
2014-03-27 11:06:48 +0500

    wget the files over a site.

---
+++ php_weget.php
@@ -0,0 +1,7 @@
+<?php
+
+// wget the files over a site. also this command can aldo work stand alone as its not
+//php fuction, its a linux command.
+
+exec("wget --http-user=user--http-user=pass http://www.yoursite/file.xml");
+?>
