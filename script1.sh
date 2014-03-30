861b474347d317c2f2b989523c74081471569044
Muzzamil Hussain <cc.muzz@gmail.com>
2014-03-30 23:17:05 +0500

    a simple bash script to fetch currently connected users on a Linux based machine

---
+++ script1.sh
@@ -0,0 +1,24 @@
+# ! /bin/bash
+clear
+echo  "the script starts now"
+echo "hi , $USER !"
+echo
+
+	echo "i will fetch you a list of connected users"
+	echo
+	w 
+	echo 
+
+echo " i am setting two colors now"
+COLOUR= "black"
+VALUE="9"
+
+	echo "this is a string: $COLOUR"
+	echo "and this is a number: $VALUE"
+	echo
+
+echo "i am giving you back your prompt now"
+echo
+~
+~
+~
